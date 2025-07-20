e498ba1b72e7112ad1f81a7eba86e010496d44fc
Mohamed Khassal <khassal.mohamed1@gmail.com>
2025-07-20 21:29:51 +0100

    lecture 3 end Successfully

---
+++ project1/main.c
@@ -0,0 +1,22 @@
+//
+// Created by mohamed khassal on 20/7/2025.
+//
+
+#include <stdio.h>
+
+
+
+int main ( int argc , char **argv){
+    int total ;
+   int favoriteNumber =0;
+    printf("Enter your favourite number\n");
+    scanf("%d",&favoriteNumber);
+    printf("Your favourite number %d \n",favoriteNumber);
+    printf("total %d ",total);
+    return 0;
+
+
+}
+
+
+
