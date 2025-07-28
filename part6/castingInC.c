2a3c1069970a68fdd7b26765739864eeb532214a
Mohamed Khassal <khassal.mohamed1@gmail.com>
2025-07-28 21:50:38 +0100

    finishing part 6

---
+++ part6/castingInC.c
@@ -0,0 +1,30 @@
+//
+// Created by mohamed khassal on 28/7/2025.
+//
+
+#include <stdio.h>
+
+
+int main() {
+
+    int intiger;
+
+
+    char character;
+
+    long longVal;
+
+    long long longlong;
+    double doubleval;
+
+    long double longdouble;
+
+    printf("size of int %zd\n", sizeof(intiger));
+    printf("size of char %zd\n", sizeof(character));
+    printf("size of long %zd\n", sizeof(longVal));
+    printf("size of long long %zd\n", sizeof(longlong));
+    printf("size of double %zd\n", sizeof(doubleval));
+    printf("size of long double %zd\n", sizeof(long double));
+
+    return 0;
+}
