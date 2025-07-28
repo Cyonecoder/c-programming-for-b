a14a4bf1396df2b6c13efbfb2ef5046387f21586
Cyonecoder <khassal.mohamed1@gmail.com>
2025-07-28 20:49:17 +0100

    challenge arithmetic done

--- challenges/arithmeticchallenge1.c
+++ challenges/arithmeticchallenge1.c
@@ -5,15 +5,16 @@
 #include <stdbool.h>
 
 
-int main(void){
-    int minutes = 60*24;
-    int minutes_in_year= 60 *24*365;
+int main() {
+    long long minutes = 60LL * 24 * 739465LL;  // Use LL for large literals
+    long long minutes_in_year = 60LL * 24 * 365;   // Should also be long long
 
-    int years =minutes/minutes_in_year;
-    int days =(minutes%minutes_in_year)/(60*24);
-    printf("minutes in year %d \n",minutes_in_year);
+    long long years = minutes / minutes_in_year;
+    long long days = (minutes % minutes_in_year) / (60 * 24);
 
-printf("years is %d years\n",years);
-    printf("days is %d days\n",days);
+    printf("Minutes in a year: %lld\n", minutes_in_year);
+    printf("Years: %lld years\n", years);
+    printf("Days: %lld days\n", days);
 
+    return 0;
 }
