f28a4cdd114525af319b9bb49e10818777e5c5dc
Cyonecoder <khassal.mohamed1@gmail.com>
2025-08-04 22:20:23 +0100

    first on function lectures

---
+++ part9/functionsDeclaration.c
@@ -0,0 +1,28 @@
+//
+// Created by NBJ on 8/4/2025.
+//
+
+
+#include <stdio.h>
+
+void add();
+void mutilplyTwoFunctions(int x , int y) {
+int result   = x*y;
+
+    printf("The product of %d and %d is: %d\n",x,y,result);
+
+
+}
+
+int main() {
+
+add();
+mutilplyTwoFunctions(10,20);
+
+    return 0 ;
+}
+
+
+void add() {
+
+}
